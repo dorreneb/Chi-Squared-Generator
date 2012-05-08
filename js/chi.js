729b9a33fe1db2b9782ea9ad73acadc84cc95aea
Dorrene Brown <dorreneb@gmail.com>
2012-05-08 02:21:11 -0400

    Cleaned up CSS, made options links more usable, and fixed the row assignations

--- js/chi.js
+++ js/chi.js
@@ -1,13 +1,38 @@
+//initialize variables 
+function init() {
+	window.numCols = 0;
+}
+window.onload = init;
+
+
+
 function addRow() {
+	var title = "Row Title"
+	//add the row to the table
 	$('#chi-table tr:last').after('<tr></tr>');
 
+	//get the row index and initialize the col index
 	var row = $('#chi-table tr:last').prevAll().length;
-	$('#chi-table tr:first').children().each(function(){
-		$('#chi-table tr:last').append("<td class='row"+row+"'>" + row + '</td>');
-	});
+	
+	//add the cells to the row and label them appropriately.
+	var i;
+	for (i=1; i <= window.numCols; i++) {
+		$('#chi-table tr:last').append("<td class='row"+row+" col"+i+"'>" + "_"+ '</td>');
+	}
+
+	//if this is the first row, then shift all the headers over 1 to make room for the title
+	if (row == 1) {
+		$("#chi-table tr:first").prepend("<th></th>");
+	}
+	//prepend the title label
+	$('#chi-table tr:last').prepend("<td class='rowtitle'>" + title + '</td>');
+	
 }
 
-function addColumn(title) {
+function addColumn() {
+	//get title (TODO: make this suck way less)
+	var title = "Title";//prompt("Enter Cell Title");
+
 	//put header in table
 	$("#chi-table tr:first").append("<th>" + title + "</th>");
 	
@@ -19,6 +44,8 @@ function addColumn(title) {
 		//get the first class attribute of the column before it (the row)
 		var rowClass = $(this).children().last().attr('class').split(" ")[0];
 		
-		$(this).append("<td class='"+rowClass+" col" + col + "'></td>");
+		$(this).append("<td class='"+rowClass+" col"+window.numCols+ "'></td>");
 	});
+	
+	window.numCols++;
 }
