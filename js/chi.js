7c9e4430786392a0f58e7adb8e299a47c21437d9
Dorrene Brown <dorreneb@gmail.com>
2012-05-08 15:42:50 -0400

    Yay, chi-square is generated!

--- js/chi.js
+++ js/chi.js
@@ -1,8 +1,10 @@
 //initialize variables 
 function init() {
 	window.numCols = 0;
+	window.numRows = 0;
 }
 window.onload = init;
+
 function addRow() {
 	//get title (TODO: make this suck way less)
 	var title = prompt("Enter Cell Title");
@@ -14,8 +16,7 @@ function addRow() {
 	var row = $('#chi-table tr:last').prevAll().length;
 	
 	//add the cells to the row and label them appropriately.
-	var i;
-	for (i=1; i <= window.numCols; i++) {
+	for (var i=1; i <= window.numCols; i++) {
 		$('#chi-table tr:last').append("<td class='row"+row+"_col"+i+"' onclick=\"edit('row"+row+"_col"+i+"');\">" + "_"+ '</td>');
 	}
 
@@ -23,8 +24,15 @@ function addRow() {
 	if (row == 1) {
 		$("#chi-table tr:first").prepend("<th></th>");
 	}
+	
+	//update row marginals
+	$("#row-marginals").append("<li id='rowmarginal"+window.numCols+"'>" + title+ "</li>");
+	
 	//prepend the title label
 	$('#chi-table tr:last').prepend("<td class='rowtitle'>" + title + '</td>');
+	
+	//update number of rows
+	window.numRows++;
 	
 }
 
@@ -43,12 +51,47 @@ function addColumn() {
 		$(this).append("<td class='"+rowClass+"_col"+window.numCols+ "' onclick=\"edit('row"+row+"_col"+window.numCols+"');\"></td>");
 	});
 	
+	//update Column marginals
+	$("#column-marginals").append("<li id='colmarginal"+window.numCols+"'>" + title+ "</li>");
+	
 	window.numCols++;
 }
 
 function edit(identifier) {
+	//get small and large box information
 	var small = prompt("Enter Small Box Content");
 	var large = prompt("Enter Large Box Content");
+	
+	if (is_int(small) && is_int(large)){
+		//add information to the box
+		$("."+identifier).html("<div class='small-box'>"+small+"</div><div class='large-box'>"+large+"</div>");
+		
+		//update the chi squared information
+		updateChiSquared(identifier);
+	} else {
+		alert("Inputs are invalid :(");
+	}
+}
 
-	$("."+identifier).html("<div class='small-box'>"+small+"</div><div class='large-box'>"+large+"</div>");
+function updateChiSquared(identifier) {
+	var boxId = "chi-squared"+identifier;
+	
+	//add the chi-squared bulletpoint if it doesn't exist
+	if (!$("#"+boxId).length){
+		$("#chi-squared-numbers").append("<li id='"+boxId+"'></li>");
+	}
+	
+	var big = parseInt($("."+identifier+" > .large-box").text());
+	var small = parseInt($("."+identifier+" > .small-box").text());
+	var chisquare = Math.pow((big-small), 2) / small;
+	alert(chisquare);
+}
+
+//Source: http://www.inventpartners.com/content/javascript_is_int
+function is_int(value){ 
+  if((parseFloat(value) == parseInt(value)) && !isNaN(value)){
+      return true;
+  } else { 
+      return false;
+  } 
 }
