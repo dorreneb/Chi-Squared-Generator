3eab71edbea91ec830515e50289e6dab857eaa08
Dorrene Brown <dorreneb@gmail.com>
2012-05-08 01:12:07 -0400

    Adding columns automagically assigns the cell the correct row and column class.

--- js/chi.js
+++ js/chi.js
@@ -1,15 +1,24 @@
 function addRow() {
 	$('#chi-table tr:last').after('<tr></tr>');
 
+	var row = $('#chi-table tr:last').prevAll().length;
 	$('#chi-table tr:first').children().each(function(){
-		$('#chi-table tr:last').append('<td></td>');
+		$('#chi-table tr:last').append("<td class='row"+row+"'>" + row + '</td>');
 	});
 }
 
 function addColumn(title) {
+	//put header in table
 	$("#chi-table tr:first").append("<th>" + title + "</th>");
 	
+	//get the number of columns that there are
+	var col = $('#chi-table th:last').prevAll().size() + 1;
+	
+	//add an extra cell to each row
 	$('#chi-table tr:not(:first)').each(function(){
-		$(this).append("<td></td>");
+		//get the first class attribute of the column before it (the row)
+		var rowClass = $(this).children().last().attr('class').split(" ")[0];
+		
+		$(this).append("<td class='"+rowClass+" col" + col + "'></td>");
 	});
 }
