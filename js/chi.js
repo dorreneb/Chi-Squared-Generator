6eb144ddf56feca08e2cedbd52af27fbabeafe4b
Dorrene Brown <dorreneb@gmail.com>
2012-05-08 03:37:49 -0400

    Still need to work out bugs, but you can input data.

--- js/chi.js
+++ js/chi.js
@@ -3,11 +3,10 @@ function init() {
 	window.numCols = 0;
 }
 window.onload = init;
-
-
-
 function addRow() {
-	var title = "Row Title"
+	//get title (TODO: make this suck way less)
+	var title = prompt("Enter Cell Title");
+	
 	//add the row to the table
 	$('#chi-table tr:last').after('<tr></tr>');
 
@@ -17,7 +16,7 @@ function addRow() {
 	//add the cells to the row and label them appropriately.
 	var i;
 	for (i=1; i <= window.numCols; i++) {
-		$('#chi-table tr:last').append("<td class='row"+row+" col"+i+"'>" + "_"+ '</td>');
+		$('#chi-table tr:last').append("<td class='row"+row+"_col"+i+"' onclick=\"edit('row"+row+"_col"+i+"');\">" + "_"+ '</td>');
 	}
 
 	//if this is the first row, then shift all the headers over 1 to make room for the title
@@ -31,21 +30,25 @@ function addRow() {
 
 function addColumn() {
 	//get title (TODO: make this suck way less)
-	var title = "Title";//prompt("Enter Cell Title");
+	var title = prompt("Enter Cell Title");
 
 	//put header in table
 	$("#chi-table tr:first").append("<th>" + title + "</th>");
 	
-	//get the number of columns that there are
-	var col = $('#chi-table th:last').prevAll().size() + 1;
-	
 	//add an extra cell to each row
 	$('#chi-table tr:not(:first)').each(function(){
 		//get the first class attribute of the column before it (the row)
-		var rowClass = $(this).children().last().attr('class').split(" ")[0];
+		var rowClass = $(this).children().last().attr('class').split("_")[0];
 		
-		$(this).append("<td class='"+rowClass+" col"+window.numCols+ "'></td>");
+		$(this).append("<td class='"+rowClass+"_col"+window.numCols+ "' onclick=\"edit('row"+row+"_col"+window.numCols+"');\"></td>");
 	});
 	
 	window.numCols++;
+}
+
+function edit(identifier) {
+	var small = prompt("Enter Small Box Content");
+	var large = prompt("Enter Large Box Content");
+
+	$("."+identifier).html("<div class='small-box'>"+small+"</div><div class='large-box'>"+large+"</div>");
 }
